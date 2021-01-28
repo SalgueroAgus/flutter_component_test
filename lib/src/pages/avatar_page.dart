cf9d3b28b9fbda8c276468eef505f283113e52a0
myusername <salguerocuentas@gmail.com>
2021-01-28 20:09:40 -0300

    Avatars

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class avatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(2.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.soriana.com/medias/sys_master/images/images/h3e/h01/8951923310622.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.soriana.com/medias/sys_master/images/images/h3e/h01/8951923310622.jpg'),
+          placeholder: AssetImage('assets/tenor.gif'),
+          fadeInDuration: Duration(milliseconds: 500),
+        ),
       ),
     );
   }
