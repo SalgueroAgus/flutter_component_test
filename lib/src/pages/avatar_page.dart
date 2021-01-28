223b7e4556a050c4f799471b95c8f75f9b2feee1
myusername <salguerocuentas@gmail.com>
2021-01-28 16:47:56 -0300

    navigator

---
+++ lib/src/pages/avatar_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class avatarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Avatar Page'),
+      ),
+    );
+  }
+}
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:component_test/src/utils/icono_string_util.dart';
 import 'package:component_test/src/providers/menu_provider.dart';
 
+import 'package:component_test/src/pages/alert_page.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,20 +25,25 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
     data.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right_sharp),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) {
+            return alertPage();
+          });
+          Navigator.push(context, route);
+        },
       );
       opciones..add(widgetTemp)..add(Divider());
     });
