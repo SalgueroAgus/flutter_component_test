ce1adcc5731e1a89fa685c42bb4778dda25ea863
myusername <salguerocuentas@gmail.com>
2021-01-28 19:56:17 -0300

    alert modal

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,12 +7,53 @@ class alertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                  'Este es el contenido de la caja de alerta asi que no te hagas el loco gil '),
+              FlutterLogo(
+                size: 100,
+              )
+            ],
+          ),
+          actions: [
+            FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Cancelar')),
+            FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Aceptar')),
+          ],
+        );
+      },
     );
   }
 }
